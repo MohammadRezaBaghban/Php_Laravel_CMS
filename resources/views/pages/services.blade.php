f4f8c6f2188c4b1c85b65c40e13b906b37e4d3db
Mikael <mikae.ivanov@gmail.com>
2020-04-25 12:17:57 +0200

    nav bar + some style

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'Web3Project')}}</title>
-        
-    </head>
-    <body>
-        <h1>services</h1>
-        <p>some about text</p>
-    </body>
-</html>
+@section('content')
+    <h1>{{$title}}</h1>
+    @if(count($services) > 0 )
+        <ul>
+            @foreach($services as $service)
+                <li class="list-group-item">{{$service}}</li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
