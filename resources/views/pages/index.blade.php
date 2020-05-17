bea8fe6833914b33ba67744dd3b70b429f2fc8c5
Mrbh <Mrbhmr@gmail.com>
2020-05-17 20:31:28 +0200

    some changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome to Laravel</h1>
+        <h1>Welcome To Our Laravel Protype Project</h1>
         <p>this is laravel app</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
 @endsection
